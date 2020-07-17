fda43372276513e26863a30ad8f8a03621ee8dfd
Kevin Galloway <kevin.l.galloway@gmail.com>
2020-07-17 12:39:01 -0500

    Added index view and homepage details

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,21 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title') - Business Manager</title>
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
-    </head>
-    <body>
-        @include('partials.nav')
-        <main>@yield('content')</main>
-    </body>
-</html>
+<!-- resources/views/home.blade.php -->
+<!-- Specify that we want to extend the index file -->
+@extends('index')
+<!-- Set the title content to "Home" -->
+@section('title', 'Home')
+<!-- Set the "content" section, which will replace "@yield('content')" in the index file we're extending -->
+@section('content')
+
+
+
+<div class="jumbotron text-light" style="background-image: url('https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80')">
+    <div class="container">
+            <h1 class="display-4">Welcome to my devtest site!</h1>
+            <p class="lead">Where all your fizz and buzzes come true.</p>
+            <a href="/fizzbuzz" class="btn btn-success btn-lg my-2">View FizzBuzz Program</a>
+                 
+    </div>
+</div>
+
+
+@endsection
