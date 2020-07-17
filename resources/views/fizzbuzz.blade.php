7f7dad2f30dbd5f936a1cc394281343b3c9f0433
Kevin Galloway <kevin.l.galloway@gmail.com>
2020-07-17 12:27:37 -0500

    Add views

---
+++ resources/views/fizzbuzz.blade.php
@@ -0,0 +1,10 @@
+<!-- resources/views/fizzbuzz.blade.php -->
+@extends('index')
+@section('title', 'Businesses')
+@section('content')
+<div class="container my-5">
+    <div class="row">
+       
+    </div>
+</div>
+@endsection
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand navbar-dark bg-primary">     
+    <div class="navbar-nav w-100">
+        <a class="navbar-brand text-color" href="/">Appointment Maker</a>
+        <a class="nav-item nav-link" href="/fizzbuzz">Fizz Buzz</a>
+        
+    </div>
+</nav>
