07143fef1d61321f7fc81c76caf0a4b95ad22da3
Kevin Galloway <kevin.l.galloway@gmail.com>
2020-07-17 12:42:01 -0500

    changed wording of homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 <div class="jumbotron text-light" style="background-image: url('https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80')">
     <div class="container">
             <h1 class="display-4">Welcome to my devtest site!</h1>
-            <p class="lead">Where all your fizz and buzzes come true.</p>
+            <p class="lead">Where all your fizzing and buzzing comes true.</p>
             <a href="/fizzbuzz" class="btn btn-success btn-lg my-2">View FizzBuzz Program</a>
                  
     </div>
