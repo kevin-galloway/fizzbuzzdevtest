7bed4625f1b81100e7a2727f54ea60b32680dc33
Kevin Galloway <kevin.l.galloway@gmail.com>
2020-07-17 12:40:44 -0500

    changed navbar title

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand navbar-dark bg-primary">     
     <div class="navbar-nav w-100">
-        <a class="navbar-brand text-color" href="/">Appointment Maker</a>
+        <a class="navbar-brand text-color" href="/">Developer Test</a>
         <a class="nav-item nav-link" href="/fizzbuzz">Fizz Buzz</a>
         
     </div>
