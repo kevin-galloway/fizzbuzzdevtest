ddd1c275800eac023f381610896ebef49b3a5ea4
Kevin Galloway <kevin.l.galloway@gmail.com>
2020-07-17 12:30:37 -0500

    Added FizzBuzz Program To View

--- resources/views/fizzbuzz.blade.php
+++ resources/views/fizzbuzz.blade.php
@@ -1,10 +1,20 @@
 <!-- resources/views/fizzbuzz.blade.php -->
 @extends('index')
-@section('title', 'Businesses')
+@section('title', ' FizzBuzz')
 @section('content')
 <div class="container my-5">
     <div class="row">
-       
+        @for($i=1; $i<=100; $i++)
+            @if ($i % 15 == 0)
+                FizzBuzz
+            @elseif ($i % 3 == 0)
+                Fizz
+            @elseif ($i % 5 ==0)
+                Buzz
+            @else 
+                {{ $i }}
+            @endif
+        @endfor
     </div>
 </div>
 @endsection
